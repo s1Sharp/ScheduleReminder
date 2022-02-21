6acc578cee1f222c39d54f7be0d0662039dc7b76
Ildar <45717260+Karantir73@users.noreply.github.com>
2022-02-21 22:21:51 +0300

    make skeleton of app

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+# API_TOKEN = os.environ['BOT_TOKEN']
+API_TOKEN = ''
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from aiogram.utils import executor
+
+from bot import dp
+
+
+def main():
+    print('start bot')
+    executor.start_polling(dp, skip_updates=True)
+
+
 if __name__ == '__main__':
-    print("hello")
+    main()
