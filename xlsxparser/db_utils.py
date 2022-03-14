b8e16b6df90c69d2d91979a14319fb7bb05b2fdf
s1Sharp <maksim.carkov.201300@gmail.com>
2022-03-14 03:56:55 +0300

    db func save/get(by group) schd string

--- xlsxparser/db_utils.py
+++ xlsxparser/db_utils.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
 
-db_ip = "localhost"
-db_port = 27017
-db_name = "timetable_db"
-time_collection = "time"
-groups_collection = "groups"
-subs_collection = "subscriptions"
+db_ip               = "localhost"
+db_port             = 27017
+db_name             = "timetable_db"
+time_collection     = "time"
+groups_collection   = "groups"
+subs_collection     = "subscriptions"
+schedule_collection = "schedule"
 
 invalid_returned_id = -1
 
@@ -29,23 +30,41 @@ class MongodbService(object):
     def get_data(self, colname):
         return list(self._db[str(colname)].find())
 
-    def save_data_time(self, data):
+    def save_data_time(self, data) -> int:
         try:
             return self._db[time_collection].insert_one(data).inserted_id
         except Exception as e:
             print(e)
             return invalid_returned_id
 
-    def save_data_groups(self, data):
+    def save_data_groups(self, data) -> int:
         try:
             return self._db[groups_collection].insert_one(data).inserted_id
         except Exception as e:
             print(e)
             return invalid_returned_id
 
-    def save_data_subs(self, data):
+    def save_data_subs(self, data) -> int:
         try:
             return self._db[subs_collection].insert_one(data).inserted_id
         except Exception as e:
             print(e)
             return invalid_returned_id
+
+    def save_data_schedule(self, data) -> bool:
+        try:
+            self._db[schedule_collection].delete_many({})
+            self._db[schedule_collection].insert_many(data)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+            
+    def get_data_by_group_key(self, group_key) -> str:
+        try:
+            db_ret = self._db[schedule_collection].find_one({'group_key': group_key})
+            day_string = db_ret['day_str']
+            return day_string
+        except Exception as e:
+            print(e)
+            return ''
