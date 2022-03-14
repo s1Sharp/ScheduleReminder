4ca1348279985e6a7a64a681acef16f630902087
s1Sharp <maksim.carkov.201300@gmail.com>
2022-03-14 03:58:23 +0300

    app db test

--- xlsxparser/app_db.py
+++ xlsxparser/app_db.py
@@ -1,5 +1,6 @@
 from db_utils import MongodbService, invalid_returned_id
-from xlsxparser import parse_time_idx, parse_group_idx
+from xlsxparser import parse_current_schedule
+
 
 storage = MongodbService.get_instance()
 
@@ -8,14 +9,22 @@ if __name__ == "__main__":
     gr = ["09-811", "09-812"]
     data_subs = {user_id: [gr]}
     if storage.save_data_subs(data_subs) != invalid_returned_id:
-        print("Data groups were successfully saved")
+        print("Data groups were successfully saved") 
+    print(storage.get_data_by_group_key("09-102 (2)"))
 
-    data_time = parse_time_idx()
+    schedule_strings = parse_current_schedule()
+
+    if storage.save_data_schedule(schedule_strings) != invalid_returned_id:
+        print("Data groups were successfully saved") 
+
+
+
+'''    data_time = parse_time_idx()
     data_group = parse_group_idx()
 
     if storage.save_data_time(data_time) != invalid_returned_id:
         print("Data time were successfully saved")
     if storage.save_data_groups(data_group) != invalid_returned_id:
-        print("Data groups were successfully saved")
+        print("Data groups were successfully saved")'''
 
 #   data_subs = {1:[10],2:[3,10], 1:[12]}
