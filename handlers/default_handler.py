ab830b387b331a3fa287cd19080f26378dc4ede1
Ildar <45717260+Karantir73@users.noreply.github.com>
2022-03-15 23:14:57 +0300

    add default_handler

---
+++ handlers/default_handler.py
@@ -0,0 +1,7 @@
+from aiogram import types
+from bot import dp
+
+
+@dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
+async def cmd_default_handler(message: types.Message):
+    await message.answer("извините, не могу разобрать вашу команду")
