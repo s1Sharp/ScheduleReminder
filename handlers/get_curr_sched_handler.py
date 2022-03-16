ff0af34eb3d8c583de952708d315c8d33f9f0731
s1Sharp <maksim.carkov.201300@gmail.com>
2022-03-16 05:33:19 +0300

    set sub group state

--- handlers/get_curr_sched_handler.py
+++ handlers/get_curr_sched_handler.py
@@ -2,35 +2,51 @@ import re
 from aiogram import types
 
 from bot import dp
-from keyboards import get_cur_sched_button_name
+from keyboards import get_cur_sched_button_name, main_menu_keyboard, set_subgroup_keyboard
 from states import MainForm
-from xlsxparser import xlsxparser
+from xlsxparser import xlsx_parser
+from xlsxparser import xlsx_env
 
+INVALID_TEXT = ['\n', '', None]
 
 @dp.message_handler(lambda message: message.text == get_cur_sched_button_name,
                     state="*",
                     content_types=types.ContentTypes.TEXT)
 async def cmd_get_curr_sched(message: types.Message):
-    await MainForm.get_schedule.set()
-    await message.answer("введите номер группы")
-
-
-def is_valid_group_number(group_number):
-    return re.match(r"^\d{2}-\d{3}$", group_number)
-
+    await MainForm.set_subgroup.set()
+    await message.answer("введите номер группы",reply_markup=types.ReplyKeyboardRemove())
 
 @dp.message_handler(lambda message: is_valid_group_number(message.text),
-                    state=[MainForm.get_schedule],
+                    state=[MainForm.set_subgroup],
+                    content_types=types.ContentTypes.TEXT)
+async def cmd_get_curr_sched(message: types.Message):
+    await MainForm.get_schedule.set()
+    state = dp.get_current().current_state()
+    await state.update_data(group_number=message.text)
+    await message.answer("введите номер подгруппы", reply_markup=set_subgroup_keyboard)
+
+def is_valid_group_number(group_number):
+    fixed_groups = [elem.split(' ')[0] for elem in xlsx_env.dgroup]
+    return re.match(r"^\d{2}-\d{3}$", group_number) and (group_number in fixed_groups)
+
+@dp.message_handler(state=[MainForm.get_schedule],
                     content_types=types.ContentTypes.TEXT)
 async def cmd_get_curr_sched(message: types.Message):
     await MainForm.menu.set()
-    group_number = message.text
-    schedule = xlsxparser.storage.get_data_by_group_key(group_number)
-    await message.answer(schedule, parse_mode='html')
+    state = dp.get_current().current_state()
+    group_number = await state.get_data()
+    subgroup_number = message.text
+    schedule = xlsx_parser.storage.get_data_by_group_key(group_number['group_number'], subgroup_number)
+    schedule = schedule.split(xlsx_env.KEY_WORD_NEXT_DAY)
+    for text in schedule:
+        if text not in INVALID_TEXT:
+            await message.answer(text, parse_mode='html', reply_markup=main_menu_keyboard)
+        
 
 
 @dp.message_handler(lambda message: not is_valid_group_number(message.text),
-                    state=[MainForm.get_schedule],
+                    state=[MainForm.set_subgroup],
                     content_types=types.ContentTypes.TEXT)
 async def cmd_incorrect_group_number(message: types.Message):
-    await message.reply("формат номер группы некорректен, требуется следующий формат xx-xxx")
+    await MainForm.set_subgroup.finish()
+    await message.reply("формат номер группы некорректен, требуется следующий формат xx-xxx", reply_markup=main_menu_keyboard)
