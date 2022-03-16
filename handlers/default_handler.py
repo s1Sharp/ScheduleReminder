aefa5468c0b8b86e2f65ab98b8f45caf64c49322
s1Sharp <maksim.carkov.201300@gmail.com>
2022-03-16 05:32:58 +0300

    update default handler

--- handlers/default_handler.py
+++ handlers/default_handler.py
@@ -4,4 +4,4 @@ from bot import dp
 
 @dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
 async def cmd_default_handler(message: types.Message):
-    await message.answer("извините, не могу разобрать вашу команду")
+    await message.answer("извините, не могу разобрать вашу команду", reply_markup=types.ReplyKeyboardRemove())
