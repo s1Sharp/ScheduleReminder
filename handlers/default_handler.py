0b7a34169a19d24af4a3ec0823294f69921aa2b3
s1Sharp <maksim.carkov.201300@gmail.com>
2022-03-16 06:59:59 +0300

    update keyboard

--- handlers/default_handler.py
+++ handlers/default_handler.py
@@ -1,7 +1,9 @@
 from aiogram import types
 from bot import dp
 
+from keyboards import main_menu_keyboard
+
 
 @dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
 async def cmd_default_handler(message: types.Message):
-    await message.answer("извините, не могу разобрать вашу команду", reply_markup=types.ReplyKeyboardRemove())
+    await message.answer("извините, не могу разобрать вашу команду", reply_markup=main_menu_keyboard)
--- handlers/get_curr_sched_handler.py
+++ handlers/get_curr_sched_handler.py
@@ -49,4 +49,4 @@ async def cmd_get_curr_sched(message: types.Message):
                     content_types=types.ContentTypes.TEXT)
 async def cmd_incorrect_group_number(message: types.Message):
     await MainForm.menu.set()
-    await message.reply("номер группы некорректен, требуется следующий формат xx-xxx", reply_markup=main_menu_keyboard)
+    await message.reply("номер группы некорректен, требуется следующий формат\n <b>xx-xxx</b>", parse_mode='html', reply_markup=main_menu_keyboard)
