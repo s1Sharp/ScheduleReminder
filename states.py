4fc263c7f34196b821997801d3be2cfd69b03dfd
s1Sharp <maksim.carkov.201300@gmail.com>
2022-03-16 05:35:00 +0300

    add state for subfroup

--- states.py
+++ states.py
@@ -3,5 +3,6 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class MainForm(StatesGroup):
     menu = State()
+    set_subgroup = State()
     get_schedule = State()
     sub_schedule = State()
