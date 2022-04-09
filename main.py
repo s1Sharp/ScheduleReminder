5cea0ae19c81aa343ea3407eee4f3ab5ec714c5f
Ildar <45717260+Karantir73@users.noreply.github.com>
2022-04-10 00:26:04 +0300

    update timer after app restart

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import handlers
 from log import setup_logging
 from refresh_schedule import init_refresh_schedule_job
 from config import SCHEDULE_UPDATE_FREQUENCY_SEC
+from handlers.timer import CUSTOM_TIMER
 
 
 def main():
     setup_logging()
     logging.info("init bot")
     init_refresh_schedule_job(SCHEDULE_UPDATE_FREQUENCY_SEC)
+    CUSTOM_TIMER.update_timer()
     executor.start_polling(dp, skip_updates=True)
 
 
